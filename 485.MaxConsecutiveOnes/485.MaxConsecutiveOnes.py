0d900cef120f7f996dcc6678dc9599043b9a4d18
Logan <loganvoravong@gmail.com>
2024-09-10 11:24:32 -0400

    Time 347ms Beats 11.17% Memory 13.33MB Beats 24.97% O(N)

--- 485.MaxConsecutiveOnes/485.MaxConsecutiveOnes.py
+++ 485.MaxConsecutiveOnes/485.MaxConsecutiveOnes.py
@@ -3,11 +3,16 @@ class Solution(object):
         l = 0
         r = 0
         best_max = 0
-        while r < len(nums):
-            if nums[r] == 1:
+        for num in nums:
+            #Expand Window - Till we see a 0
+            if num == 1:
+                #Process Window
                 best_max = max(best_max, r - l + 1)
                 r += 1
             else:
+                #Contract Window - Till we see a 0
                 r += 1
                 l = r
         return best_max
+
+    
