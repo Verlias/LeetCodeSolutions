e52bdc477fca3a8be59a57b8069a7789576a196d
Logan <loganvoravong@gmail.com>
2024-09-19 17:37:03 -0400

    Time 27ms 26.25% Memory 11.98MB Beats 20.63%

--- 153.FindMinimuminRotatedSortedArray/153.FindMinimuminRotatedSortedArray.py
+++ 153.FindMinimuminRotatedSortedArray/153.FindMinimuminRotatedSortedArray.py
@@ -2,14 +2,11 @@ class Solution(object):
     def findMin(self, nums):
         l = 0
         r = len(nums) - 1
-        while l <= r:
+        while l < r:
             mid = (r + l) // 2
-            if l == r:
-                return nums[l]
-            elif nums[mid] > nums[r]:
+            if nums[mid] > nums[r]:
                 l = mid + 1
             else:
                 r = mid
-        return nums[l] 
-
+        return nums[l]
 
